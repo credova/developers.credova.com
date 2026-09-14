47ecfdee3c401217c41c6cf6e02233ee6f9a2036
Gisely Borges <gborges@credova.com>
2026-09-14 16:25:54 -0300

    merge ACH payout menu

--- sidebars.ts
+++ sidebars.ts
@@ -67,7 +67,7 @@ const sidebars: SidebarsConfig = {
       customProps: {
         icon: "/img/icons/money-bill-wave.svg",
       },
-      items: ["guides/payouts/process-card-payouts", "guides/payouts/process-ach-payouts", "guides/payouts/process-instant-payouts", "guides/payouts/cancel-payouts"],
+      items: ["guides/payouts/process-card-payouts", "guides/payouts/process-ach-payouts", "guides/payouts/cancel-payouts"],
     },
     {
       type: "category",
